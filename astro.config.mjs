09657ca8c23e916d0697c3459375836f6108fed8
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-10-11 19:27:52 -0400

    Day1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://innovative-roofing.com/',
+  integrations: [tailwind(), compress(), robotsTxt(), sitemap()]
 });
